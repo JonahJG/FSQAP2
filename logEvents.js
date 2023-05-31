87f0c1261405dfe4d303abbc6c188dd355f3bb86
Jonah Greening <116300133+JonahJG@users.noreply.github.com>
2023-05-31 12:55:30 -0230

    Added comments, questionnaire, NOW QAP is complete

--- logEvents.js
+++ logEvents.js
@@ -6,30 +6,43 @@
     // Date revised:
     // **********************************
 
-const { format, getYear } = require('date-fns');
-const { v4: uuid } = require('uuid');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-const path = require('path');
-
-const logEvents = async (event, level, message) => {
-  const dateTime = `${format(new Date(), 'yyyy-MM-dd\HH:mm:ss')}`;
-
-  const logEvent = `${dateTime}\t${level}\t${event}\t${message}\t${uuid()}`;
-  console.log(logEvent);
-
-  try {
-    const currFolder = 'logs/' + getYear(new Date());
-    if (!fs.existsSync(path.join(__dirname, currFolder))) {
-      console.log(`Creating folder: ${currFolder}`);
-      await fsPromises.mkdir(path.join(__dirname, currFolder), { recursive: true });
-    }
-
-    const fileName = `${format(new Date(), 'yyyy-MM-dd')}_http_events.log`;
-    await fsPromises.appendFile(path.join(__dirname, currFolder, fileName), logEvent + '\n');
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-module.exports = logEvents;
+    // Import required modules
+    const { format, getYear } = require('date-fns');
+    const { v4: uuid } = require('uuid');
+    const fs = require('fs');
+    const fsPromises = require('fs').promises;
+    const path = require('path');
+    
+    // Function to handle logging events
+    const logEvents = async (event, level, message) => {
+      // Get the current date and time in the desired format
+      const dateTime = `${format(new Date(), 'yyyy-MM-dd\HH:mm:ss')}`;
+    
+      // Create the log event string with the provided event, level, message, and a unique identifier
+      const logEvent = `${dateTime}\t${level}\t${event}\t${message}\t${uuid()}`;
+      
+      // Log the event to the console
+      console.log(logEvent);
+    
+      try {
+        // Create the folder path based on the current year
+        const currFolder = 'logs/' + getYear(new Date());
+    
+        // Check if the folder exists, and if not, create it
+        if (!fs.existsSync(path.join(__dirname, currFolder))) {
+          console.log(`Creating folder: ${currFolder}`);
+          await fsPromises.mkdir(path.join(__dirname, currFolder), { recursive: true });
+        }
+    
+        // Create the log file name based on the current date
+        const fileName = `${format(new Date(), 'yyyy-MM-dd')}_http_events.log`;
+    
+        // Append the log event to the log file
+        await fsPromises.appendFile(path.join(__dirname, currFolder, fileName), logEvent + '\n');
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    
+    // Exporting the logEvents function to be used in other modules
+    module.exports = logEvents;
--- routes.js
+++ routes.js
@@ -6,58 +6,67 @@
     // Date revised:
     // **********************************
 
-const fs = require('fs');
+    // Import required modules - filesystem
+    const fs = require('fs');
 
-function indexPage(path, res) {
-    if(DEBUG) console.log(`index.html page was requested`);
-    displayFile(path, res);
-}
-
-function contactPage(path, res) {    
-    if(DEBUG) console.log(`contact.html page was requested`);
-    displayFile(path, res);
-}
-
-function FourOhFourPage(path, res) {
-    if(DEBUG) console.log(`404.html page was requested`);
-    displayFile(path, res);
-}
-
-function weatherPage(path, res) {
-    if(DEBUG) console.log(`weather.html page was requested`);
-    displayFile(path, res);
-}
-
-function statsPage(path, res) {
-    if(DEBUG) console.log(`stats.html page was requested`);
-    displayFile(path, res);
-}       
-
-function petsPage(path, res) {  
-    if(DEBUG) console.log(`pets.html page was requested`);
-    displayFile(path, res);
-}   
-
-
-function displayFile(path, res) {
-    fs.readFile(path, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end();
-        } else {
-            if(DEBUG) console.log(`File read successfully`);
-            res.writeHead(res.statusCode, {'Content-Type': 'text/html'});
-            res.write(data);
-            res.end();
-        }
-    });
-}
-
-module.exports = {  
-    indexPage,  
-    contactPage,
-    FourOhFourPage,
-    weatherPage,
-    statsPage,
-    petsPage
-};
+    // Function to handle serving the index.html page
+    function indexPage(path, res) {
+        if(DEBUG) console.log(`index.html page was requested`);
+        displayFile(path, res);
+    }
+    
+    // Function to handle serving the contact.html page
+    function contactPage(path, res) {    
+        if(DEBUG) console.log(`contact.html page was requested`);
+        displayFile(path, res);
+    }
+    
+    // Function to handle serving the 404.html page
+    function FourOhFourPage(path, res) {
+        if(DEBUG) console.log(`404.html page was requested`);
+        displayFile(path, res);
+    }
+    
+    // Function to handle serving the weather.html page
+    function weatherPage(path, res) {
+        if(DEBUG) console.log(`weather.html page was requested`);
+        displayFile(path, res);
+    }
+    
+    // Function to handle serving the stats.html page
+    function statsPage(path, res) {
+        if(DEBUG) console.log(`stats.html page was requested`);
+        displayFile(path, res);
+    }       
+    
+    // Function to handle serving the pets.html page
+    function petsPage(path, res) {  
+        if(DEBUG) console.log(`pets.html page was requested`);
+        displayFile(path, res);
+    }   
+    
+    // Function to read and display a file
+    function displayFile(path, res) {
+        fs.readFile(path, (err, data) => {
+            if(err) {
+                console.log(err);
+                res.end();
+            } else {
+                if(DEBUG) console.log(`File read successfully`);
+                res.writeHead(res.statusCode, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            }
+        });
+    }
+    
+    // Exporting the functions as an object to be used in other modules
+    module.exports = {  
+        indexPage,  
+        contactPage,
+        FourOhFourPage,
+        weatherPage,
+        statsPage,
+        petsPage
+    };
+    
